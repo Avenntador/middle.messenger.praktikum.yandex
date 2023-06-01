139674b8578e199463eb8474e5951143d1d55efb
Bulat <bulat40k@gmail.com>
2023-06-01 21:33:38 +0300

    docker

--- test/babel-register.js
+++ test/babel-register.js
@@ -9,3 +9,4 @@ register({ extensions: ['.ts', '.js'] });
 const dom = new JSDOM('<div id="root"><div>', { url: 'http://localhost' });
 global.window = dom.window;
 global.document = dom.window.document;
+global.DocumentFragment = dom.DocumentFragment;
